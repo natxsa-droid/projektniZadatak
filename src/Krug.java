85d515d98842adfc5451ebb47cfb71bb80959399
natxsa-droid <natxsa@icloud.com>
2026-02-19 19:05:49 +0100

    1

---
+++ src/Krug.java
@@ -0,0 +1,11 @@
+public class Krug {
+    private String naziv;
+    private double radius;
+
+    public Krug(String naziv, double radius) {
+        this.naziv = naziv;
+        this.radius = radius;
+
+
+    }
+}

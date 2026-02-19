b6dc25c14aab21066d1d8a1eaaaab08dac2a667c
natxsa-droid <natxsa@icloud.com>
2026-02-19 19:47:57 +0100

    3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args){
         Krug krug1 = new Krug("Naziv kruga", 10);
         System.out.println(krug1.getNaziv());
+
+        Trokut trokut1 = new Trokut("Naziv trokuta", 7,8,9);
+        System.out.println(trokut1.getNaziv());
+
+
     }
+
+
 }

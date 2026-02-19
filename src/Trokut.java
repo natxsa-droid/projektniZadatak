8ade9c68e2351027f84dff454d500c8ed4457c7e
natxsa-droid <natxsa@icloud.com>
2026-02-19 20:04:39 +0100

    3

--- src/Trokut.java
+++ src/Trokut.java
@@ -2,7 +2,7 @@ public class Trokut {
     private String naziv;
     private double a, b, c;
 
-    public Trokut (String naziv; double a, double b, double c){
+    public Trokut (String naziv, double a, double b, double c) {
         this.naziv = naziv;
         this.a = a;
         this.b = b;

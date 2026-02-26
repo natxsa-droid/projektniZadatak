2d89a779e94e58f75d27442e25b2f328516e52f9
natxsa-droid <natxsa@icloud.com>
2026-02-26 19:19:54 +0100

    3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Object> listaLikova = new ArrayList<>();
 
         System.out.println("Unesi naziv kruga:");
         String nazivKruga = scanner.nextLine();
@@ -23,16 +25,19 @@ public class Main {
         double b = scanner.nextDouble();
         System.out.println("Unesi stranicu c za trokut:");
         double c = scanner.nextDouble();
+        Trokut trokut1 = new Trokut(nazivTrokuta, a, b, c);
+
+        System.out.println("Unesi naziv pravokutnika:");
+        scanner.nextLine();
+        String nazivP = scanner.nextLine();
 
         System.out.println("Unesi stranicu a za pravokutnik:");
         double a2 = scanner.nextDouble();
         System.out.println("Unesi stranicu b za pravokutnik:");
         double b2 = scanner.nextDouble();
 
-
-        Pravokutnik pravokutnik1 = new Pravokutnik("Naziv pravokutnika", 6, 7);
-        System.out.println(pravokutnik1.getNaziv());
-
-
+        Pravokutnik pravokutnik1 = new Pravokutnik(nazivP, a2, b2);
+        System.out.println("Naziv pravokutnika je: " + pravokutnik1.getNaziv());
+        System.out.println(pravokutnik1);
     }
 }

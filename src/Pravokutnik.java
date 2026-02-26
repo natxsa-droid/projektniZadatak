f71e736b29e97e462bffb9b23564fd49a1006fe2
natxsa-droid <natxsa@icloud.com>
2026-02-26 19:24:37 +0100

    3

--- src/Pravokutnik.java
+++ src/Pravokutnik.java
@@ -2,16 +2,19 @@ public class Pravokutnik {
     private String naziv;
     private double a, b;
 
-    public Pravokutnik(String naziv, double a, double b){
+    public Pravokutnik(String naziv, double a, double b) {
         this.naziv = naziv;
         this.a = a;
-        this.b = b; }
+        this.b = b;
+    }
 
-    public String getNaziv(){
-    return naziv; }
+    public String getNaziv() {
+        return naziv;
+    }
 
     public double getA() {
-        return a; }
+        return a;
+    }
 
     public double getB() {
         return b;
@@ -25,4 +28,5 @@ public class Pravokutnik {
     public String toString() {
         return "Lik: " + naziv + " (Pravokutnik), Stranice: " + a + ", " + b +
                 ", Površina: " + String.format("%.2f", getPovrsina());
+    }
 }
